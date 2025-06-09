84c3aaaef37f60bcc0a6d53a52d5453f9ec2bf97
khushisharma123456 <144713310+khushisharma123456@users.noreply.github.com>
2025-06-10 00:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from urllib.parse import urlencode
 import base64
 from datetime import datetime, timedelta
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
